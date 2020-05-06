8c87b70c8f0dde6c9735b79ed3f1e51871d37522
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-06 23:50:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,10 +6,10 @@ def my_all?(collection)
  while i < collection.length
    block_return_values <<  yield(collection[i])
     i += 1
-  end
-  if block_return_values.include(false)
+ end
+ if block_return_values.include?(false)
     false
-  else
+ else
     true
   end
 end
